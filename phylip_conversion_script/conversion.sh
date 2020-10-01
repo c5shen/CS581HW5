b397de5af884db4089304db6d76697e1d19eacf9
Chengze Shen <zhazhashen@gmail.com>
2020-10-01 15:00:40 -0400

    added evaluation

--- phylip_conversion_script/conversion.sh
+++ phylip_conversion_script/conversion.sh
@@ -2,7 +2,7 @@
 
 # 16S.M conversion
 python3 convert_fasta_to_phy.py ../../16S.M/R0/cleaned.alignment.fasta
-python3 convert_fasta_to_phy.py ../mafft/16S.M/mafft.aln.fasta
+python3 convert_fasta_to_phy.py ../MAFFT/16S.M/mafft.aln.fasta
 python3 convert_fasta_to_phy.py ../PASTA/16S.M/pastajob.marker001.cleaned.unaln.aln
 
 # 1000M1 conversion
@@ -11,7 +11,7 @@ rep=9
 for i in $(seq 0 $rep); do
         python3 convert_fasta_to_phy.py $prefix1$i/rose.aln.true.fasta
         python3 convert_fasta_to_phy.py ../PASTA/1000M1/R$i/pastajob.marker001.rose.unaln.true.aln
-        python3 convert_fasta_to_phy.py ../mafft/1000M1/R$i/mafft.aln.fasta
+        python3 convert_fasta_to_phy.py ../MAFFT/1000M1/R$i/mafft.aln.fasta
 done
 
 
@@ -20,6 +20,6 @@ prefix2=$(pwd)/../../1000M4/1000M4/R
 for i in $(seq 0 $rep); do
         python3 convert_fasta_to_phy.py $prefix2$i/rose.aln.true.fasta
         python3 convert_fasta_to_phy.py ../PASTA/1000M4/R$i/pastajob.marker001.rose.unaln.true.aln
-        python3 convert_fasta_to_phy.py ../mafft/1000M4/R$i/mafft.aln.fasta
+        python3 convert_fasta_to_phy.py ../MAFFT/1000M4/R$i/mafft.aln.fasta
 done
 
